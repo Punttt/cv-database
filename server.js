f1f51b803ff0c239142137763c09a4aba39d7d2d
Pontus Johansson <johanssonp998@gmail.com>
2026-04-01 13:25:28 +0200

    påbörjar med server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const { Client } = require("pg");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended : true }));
+
+// Anslut till databasen
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
