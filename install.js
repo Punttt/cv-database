3ae53d1bac91edad6ac776c91970b8a4ddf53553
Pontus Johansson <johanssonp998@gmail.com>
2026-04-01 13:20:22 +0200

    first commit

---
+++ install.js
@@ -0,0 +1,44 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+// Anslut till databasen
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect((err)=>{
+    if(err){
+        console.log(`Connection error: ${err}`);
+    } else {
+        console.log(`Connected to database!`);
+        createTables();
+    }
+});
+
+async function createTables() {
+    try{
+        const res = await client.query(`
+                DROP TABLE IF EXISTS cv_database;
+                CREATE TABLE IF NOT EXISTS cv_database(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    code VARCHAR(255) NOT NULL,
+                    url VARCHAR(255) NOT NULL,
+                    progression VARCHAR(1) NOT NULL,
+                    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+            `)
+            console.log(res);
+    } catch(err) {
+        console.error(err);
+    } finally {
+        await client.end();
+    }
+}
