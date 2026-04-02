d9a4c584b38f3f4911ec7caf0013900bbf7e0306
Pontus Johansson <johanssonp998@gmail.com>
2026-04-02 10:22:25 +0200

    påbörjar med post för formuläret

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ app.get("/", (req, res)=>{
     res.render("index")
 });
 
+app.post("/", async(req, res)=>{
+    const name = req.body.name;
+    const code = req.body.code;
+    const url = req.body.url;
+    const progression = req.body.progression;
+    const error = "";
+
+    if( name === "" || code === "" || url === "" || progression === ""){
+        return res.redirect("/");
+    } 
+
+    try{
+
+        res.redirect("/");
+    } catch(error){
+        console.log(error);
+        res.redirect("/");
+    } 
+});
+
 // Startar applikationen
 app.listen(process.env.PORT, ()=>{
     console.log(`Server startad på http://localhost:${process.env.PORT}`);
